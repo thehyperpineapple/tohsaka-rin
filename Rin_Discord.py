5ea3d505b893373bbb1f83fe647bc96ff9444c07
thehyperpineapple <62801500+thehyperpineapple@users.noreply.github.com>
2020-03-29 21:39:56 +0530

    Update Rin_Discord.py

--- Rin_Discord.py
+++ Rin_Discord.py
@@ -7,7 +7,7 @@ client = commands.Bot(command_prefix="-")
 
 @client.event
 async def on_ready():
-    await client.change_presence(status=discord.Status.online, activity=discord.Game("with Sakura"))
+    await client.change_presence(status=discord.Status.online, activity=discord.Game("with Magic"))
     print("I'm ready to help")
 
 
