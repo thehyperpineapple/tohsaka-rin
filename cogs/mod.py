8fa85508fd33c23dd820aeecde2f26e9b95069da
thehyperpineapple <62801500+thehyperpineapple@users.noreply.github.com>
2020-03-31 21:56:43 +0530

    Add files via upload

---
+++ cogs/mod.py
@@ -0,0 +1,40 @@
+import discord
+from discord.ext import commands
+
+class mod (commands.Cog):
+    def __init__(self, client):
+        self.client = client
+    
+    @commands.command()
+    @commands.has_permissions(kick_members=True)
+    async def kick(self, ctx, member : discord.Member, *, reason=None):
+        await member.kick(reason=reason)
+        await ctx.send(f"Kicked {member.mention}")
+
+    @commands.command()
+    @commands.has_permissions(ban_members=True)
+    async def ban(self, ctx, member : discord.Member, *, reason=None):
+        await member.ban(reason=reason)
+        await ctx.send(f"Banned {member.mention}")
+
+    @commands.command()
+    @commands.has_permissions(manage_messages=True)
+    async def clear(self, ctx, amount=5):
+        await ctx.channel.purge(limit=amount+1)
+
+    @commands.command()
+    @commands.has_permissions(ban_members=True)
+    async def unban(self, ctx, *, member):
+        banned_users = await ctx.guild.bans()
+        member_name,member_discriminator = member.split("#")
+        for ban_entry in banned_users:
+            user = ban_entry.user
+            if (user.name,user.discriminator) == (member_name,member_discriminator):
+                await ctx.guild.unban(user)
+                await ctx.send(f"Unbanned {user.mention}")
+                return
+
+
+
+def setup(client):
+    client.add_cog(mod(client))
