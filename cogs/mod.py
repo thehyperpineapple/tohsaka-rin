092446ec190eeea1d6dab30c920953890c7ac336
thehyperpineapple <62801500+thehyperpineapple@users.noreply.github.com>
2020-07-13 22:54:58 +0530

    Update mod.py

--- cogs/mod.py
+++ cogs/mod.py
@@ -19,7 +19,7 @@ class mod (commands.Cog):
 
     @commands.command()
     @commands.has_permissions(manage_messages=True)
-    async def clear(self, ctx, amount=5):
+    async def clear(self, ctx, amount=1):
         await ctx.channel.purge(limit=amount+1)
 
     @commands.command()
