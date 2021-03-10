e04195a7ac3d3979f2efcee491f510ed5ad28cab
thehyperpineapple <62801500+thehyperpineapple@users.noreply.github.com>
2021-03-10 19:02:50 +0530

    Update Rin_Discord.py

--- Rin_Discord.py
+++ Rin_Discord.py
@@ -127,4 +127,4 @@ for filename in os.listdir("./cogs"):
     if filename.endswith(".py"):
         client.load_extension(f"cogs.{filename[:-3]}")
 
- client.run(os.environ["TOKEN"])
+client.run(os.environ["TOKEN"])
